f277ee83bc9b5db361959eb8896c317fa5bde196
David Cannon <2680961c@student.gla.ac.uk>
2023-01-26 19:50:18 +0000

    [Lab3/Ch6] Added top categories/pages lists on home page, category page view/template, slug field to category model, more internal commentary for population script

--- populate_rango.py
+++ populate_rango.py
@@ -10,38 +10,46 @@ def populate():
         {
             'title': 'Official Python Tutorial',
             'url': 'http://docs.python.org/3/tutorial/',
+            'views': 65,
         },
         {
             'title' : 'How to Think like a Computer Scientist',
             'url': 'http://www.greenteapress.com/thinkpython/',
+            'views': 77,
         },
         {
             'title': 'Learn Python in 10 Minutes',
             'url': 'http://www.korokithakis.net/tutorials/python/',
+            'views': 16,
         },
     ]
     django_pages = [
         {
             'title': 'Official Django Tutorial',
             'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+            'views': 22,
         },
         {
             'title': 'Django Rocks',
             'url': 'http://www.djangorocks.com/',
+            'views': 97,
         },
         {
             'title': 'How to Tango with Django',
             'url': 'http://www.tangowithdjango.com/',
+            'views': 46,
         },
     ]
     other_pages = [
         {
             'title': 'Bottle',
             'url': 'http://bottlepy.org/docs/dev',
+            'views': 60,
         },
         {
             'title': 'Flask',
             'url': 'http://flask.pocoo.org',
+            'views': 53,
         },
     ]
 
@@ -63,21 +71,36 @@ def populate():
         }
     }
 
+    # For each category in our dictionary above
     for category, category_data in categories.items():
+        # Add the category to the database
         db_category = add_category(category, category_data['views'], category_data['likes'])
         for page in category_data['pages']:
-            add_page(db_category, page['title'], page['url'])
+            # Add each of the category's associated pages to the databasd
+            add_page(db_category, page['title'], page['url'], page['views'])
 
+    # Print each category and its associated pages that we've added
+    # to the database
     for db_category in Category.objects.all():
         for db_page in Page.objects.filter(category=db_category):
             print(f'- {db_category}: {db_page}')
 
 def add_page(category, title, url, views=0):
+    # Add page to database, returning a reference to the page model
+    # instance if it already exists. The get_or_create method returns
+    # a tuple with a reference to the model instance, and a boolean
+    # indicating if the model instance had to be created (false if
+    # already existed), so we get the object reference from index 0
     db_page = Page.objects.get_or_create(category=category, title=title, url=url, views=views)[0]
     db_page.save()
     return db_page
 
 def add_category(name, views, likes):
+    # Add category to database, returning a reference to the category
+    # model instance if it already exists. The get_or_create method
+    # returns a tuple with a reference to the model instance, and a
+    # boolean indicating if the model instance had to be created (false
+    # if already existed), so we get the object reference from index 0
     db_category = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     db_category.save()
     return db_category
--- rango/views.py
+++ rango/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from rango.models import Category, Page
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Retrieve the top 5 most liked categories from database
+    top_category_list = Category.objects.order_by('-likes')[:5]
+    # Retrieve the top 5 most viewed pages from database
+    top_page_list = Page.objects.order_by('-views')[:5]
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+    context_dict = {
+        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
+        'categories': top_category_list,
+        'pages': top_page_list,
+    }
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
@@ -19,3 +23,21 @@ def about(request):
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        # Query database for category with given slug
+        category = Category.objects.get(slug=category_name_slug)
+        # Retrieve all of the associated pages
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # Could not find the category with the given slug
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
